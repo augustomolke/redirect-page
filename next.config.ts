ec21ab22e77b88840bf54fa262102d2706912dbb
Augusto Otto <augustomolke@gmail.com>
2024-11-18 13:12:45 -0300

    add redirects

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/driver-panel",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/primeira-entrega",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/integracao",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
